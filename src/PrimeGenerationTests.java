eee8fa967705de71d591aa6ee691219f75b15c7c
Tai J. Enrico <taiphoon007@gmail.com>
2015-03-27 19:57:51 -0400

    IMPORTANT: Test for Int 1 Now parameterized
    Ignore all previous Task 5 test commits.
    The tests were not parameterized.

--- src/PrimeGenerationTests.java
+++ src/PrimeGenerationTests.java
@@ -1,34 +1,45 @@
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class PrimeGenerationTests {
-	PrimeGenerator pg;
+
+	private Integer inputNumber;
+	private ArrayList<Integer> expectedResult;
+	private PrimeGenerator primeGen;
 
 	@Before
-	public void setup() {
-		pg = new PrimeGenerator();
+	public void initialize() {
+		primeGen = new PrimeGenerator();
 	}
-	
-	@Test
-	public void test1() {
-		assertTrue(pg.generatePrimes(1).isEmpty());
+
+	// Each parameter should be placed as an argument here
+	// Every time runner triggers, it will pass the arguments
+	// from parameters we defined in primeNumbers() method
+	public PrimeGenerationTests(Integer inputNumber, ArrayList<Integer> expectedResult) {
+		this.inputNumber = inputNumber;
+		this.expectedResult = expectedResult;
 	}
-	
-	@Test
-	public void test2() {
-		ArrayList<Integer> result = pg.generatePrimes(2);
-		assertTrue(result.size() == 1);
-		assertTrue(result.get(0) == 2);
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> primeNumbers() {
+		List<Integer> oneResult = Arrays.asList();
+		return Arrays.asList(new Object[][] { {1, oneResult} });
 	}
-	
+
+	// This test will run 4 times since we have 5 parameters defined
 	@Test
-	public void test3() {
-		ArrayList<Integer> result = pg.generatePrimes(2);
-		assertTrue(result.size() == 1);
-		assertTrue(result.get(0) == 2);
+	public void testPrimeNumberChecker() {
+		System.out.println("Prime Numbers from 2-" + inputNumber + " (inclusive)");
+		assertEquals(expectedResult, primeGen.generatePrimes(inputNumber));
 	}
 }
