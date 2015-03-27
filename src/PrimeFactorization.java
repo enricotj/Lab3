c1af3c6afc019c22e532ab3e64737e95e8a22a29
Tai J. Enrico <taiphoon007@gmail.com>
2015-03-27 01:23:07 -0400

    Task 4.2 Test Working

--- src/PrimeFactorization.java
+++ src/PrimeFactorization.java
@@ -3,6 +3,21 @@ import java.util.ArrayList;
 public class PrimeFactorization {
 	public ArrayList<Integer> getPrimeFactorization(int x) {
 		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+		if (isPrime(x)) {
+			primeFactors.add(x);
+		}
 		return primeFactors;
+	}
+	
+	public boolean isPrime(int x) {
+		if (x == 1) {
+			return false;
+		}
+		for (int i = 2; i < x; i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
