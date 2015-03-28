d2c43a7fd04eddfd57db60605d7509a703876380
Tai J. Enrico <taiphoon007@gmail.com>
2015-03-27 20:08:39 -0400

    Task 5 Prime Input Tests (2, 3, 11) Passing

--- src/PrimeGenerator.java
+++ src/PrimeGenerator.java
@@ -4,6 +4,21 @@ import java.util.List;
 public class PrimeGenerator {
 	public List<Integer> generatePrimes(int x) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if (isPrime(x)) {
+			primes.add(x);
+		}
 		return primes;
+	}
+	
+	public boolean isPrime(int x) {
+		if (x == 1) {
+			return false;
+		}
+		for (int i = 2; i < x; i++) {
+			if (x % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
